0bbbe0cc258bcad1b2e12b45fe7128699c2f19d7
shaik Asif <smd.20sa@gmail.com>
2023-03-24 02:54:22 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>gmail clone</h1>
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
