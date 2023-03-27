e1055963c49b52b96e2f93658b32efbc9929b72d
shaik Asif <smd.20sa@gmail.com>
2023-03-27 18:48:08 +0530

    fire

--- src/firebase.js
+++ src/firebase.js
@@ -12,5 +12,7 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db };
+export { db, auth, provider };
